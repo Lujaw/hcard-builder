ae5efc50faa28aebc2dc8eb1578c2feb0ed1d9ef
Luja <>
2020-05-07 23:27:31 +1000

    HC-03: adding logger

--- src/server/index.js
+++ src/server/index.js
@@ -1,11 +1,19 @@
 import express from "express";
+import fs from "fs";
+import path from "path";
 import bodyParser from "body-parser";
+import morgan from "morgan";
 import router from "./routes";
 import { normalizePort } from "./utils";
 
 const port = normalizePort(process.env.PORT || "3000");
 const app = express();
 
+// create a write stream (in append mode)
+const accessLogStream = fs.createWriteStream(path.join(__dirname, "access.log"), { flags: "a" })
+
+// setup the logger
+app.use(morgan('combined', { stream: accessLogStream }))
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
