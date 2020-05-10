5b92989c77bf5361e783a0cb704c156a2258aba4
Luja <>
2020-05-10 10:57:17 +1000

    HC-03: enabling cors

--- src/server/index.js
+++ src/server/index.js
@@ -3,6 +3,7 @@ import fs from "fs";
 import path from "path";
 import bodyParser from "body-parser";
 import morgan from "morgan";
+import cors from "cors";
 import router from "./routes";
 import { normalizePort, errorHandler } from "./utils";
 
@@ -12,6 +13,9 @@ const app = express();
 // create a write stream in append mode
 const logFilePath = path.join(__dirname, "access.log");
 const accessLogStream = fs.createWriteStream(logFilePath, { flags: "a" });
+
+// enabling cors
+app.use(cors());
 
 // log all requests to access.log
 app.use(morgan("combined", { stream: accessLogStream }));
