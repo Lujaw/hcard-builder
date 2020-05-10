cfdf93ae23edb46aa410eef428b940818f3e7884
Luja <>
2020-05-10 15:00:25 +1000

    HC-03: adding default value for avatar img

--- src/server/routes/handlers/views.js
+++ src/server/routes/handlers/views.js
@@ -4,9 +4,7 @@ import view from "../../../shared/helpers/view";
 
 const handleViews = async (req, res, next) => {
   try {
-    console.log('views#7->>>', { url: req.url, path: req.path });
     const activeRoute = routes.find((route) => matchPath(req.url, route)) || {};
-    console.log('ssr#8->>>', { activeRoute });
     const promise = activeRoute.fetchInitialData ?
       activeRoute.fetchInitialData(req.path) :
       Promise.resolve();
